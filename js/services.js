4783b3cd648e2e27baa682ad77f3fd806bb18517
TTervuren <toerisme@tervuren.be>
2015-03-23 11:18:07 +0100

    Update services.js

--- js/services.js
+++ js/services.js
@@ -174,7 +174,7 @@ app.factory('Registration', function ($rootScope) {
                 {   id : 10,
                     registrationAllowed: true,
                     name: 'Paaseitjeswandeling Tervuren',
-                    departureHours: ['14u15', '14u30', '14u45', '15u30', '15u45'],
+                    departureHours: ['14u30', '14u45', '15u30', '15u45'],
                     formUrl: mBassadeurFormUrl,
                     redirectUrl: 'http://www.paaseitjeswandelingtervuren.be'
                 }
