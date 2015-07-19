db0cbfd4a2351b5f74a8a566731f46f8cb47d65d
Kris Lacage <kris@lacage.be>
2015-07-19 16:55:43 +0200

    added refScriptGadget

---
+++ GoogleSitesGadgets/ReferenceScriptGadget.js
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Module>
+  <ModulePrefs title="Reference other scripts on google sites page gadget" >
+    <Require feature="setprefs"/>
+  </ModulePrefs>
+
+  <Content type="html" view="home">
+    <![CDATA[
+
+      <script type="text/javascript" src="http://www.omdtervuren.be/js/iframeResizer.contentWindow.min.js"></script>  
+  
+  ]]>
+  </Content>
+
+  <Content type="html" view="configuration" preferred_height="250">
+  <![CDATA[
+
+  ]]>
+  </Content>
+</Module>
