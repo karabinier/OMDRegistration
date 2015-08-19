a647304f88b110723b2324ef82d3c003849fd1e6
TTervuren <toerisme@tervuren.be>
2015-08-19 13:12:59 +0200

    Update services.js

--- js/services.js
+++ js/services.js
@@ -123,7 +123,7 @@ app.factory('Registration', function ($rootScope) {
                 {   id : 3,
                     registrationAllowed: defaultRegistrationAllowed,
                     name: 'Een geheel van vier cottages in de Hertogenweg',
-                    departureHours: ['12u', '13u', '14u'],
+                    departureHours: ['12u', '13u'],
                     formUrl: defaultFormUrl,
                     redirectUrl: defaultRedirectUrl
                 },
